530c497f1318b50f27f7fb72e8d3d7302263ff03
Matthias Langhard <matthias@langaust.com>
2016-01-15 10:50:17 +0100

    lots of design changes

--- game.go
+++ game.go
@@ -5,33 +5,34 @@ import (
 	"log"
 )
 
+// newGameState creates the initial gameState Struct (emtpy board)
 func newGameState() gameState {
 	gs := gameState{
-		StatusMessage: "waiting for second player...",
+		StatusMessage: "Waiting to get paired",
 		Fields: []field{
-			field{Set: false, Symbol: ""},
-			field{Set: false, Symbol: ""},
-			field{Set: false, Symbol: ""},
-			field{Set: false, Symbol: ""},
-			field{Set: false, Symbol: ""},
-			field{Set: false, Symbol: ""},
-			field{Set: false, Symbol: ""},
-			field{Set: true, Symbol: "O"},
-			field{Set: false, Symbol: ""},
+			field{},field{},field{}, // row1
+			field{},field{},field{}, // row2
+			field{},field{},field{}, // row3
 		},
-		PlayerSymbols:  []string{0: "X", 1: "O"},
-		Started: false,
+		PlayerSymbols: []string{0: "X", 1: "O"},
+		Started:       false,
+		//These are not exported to JSON
+		numberOfPlayers: 0,
+		playersTurn:     0,
 	}
 	return gs
 }
 
 type gameState struct {
 	//renaming json values here to confirm the standard (lowercase var names)
-	StatusMessage   string  	`json:"statusMessage"`
-	Fields          []field 	`json:"fields"`
-	numberOfPlayers int     	`json:"-"` //not exported to JSON
-	PlayerSymbols	[]string	`json:"playerSymbols"`
-	Started		bool		`json:"started"`
+	StatusMessage string   `json:"statusMessage"`
+	Fields        []field  `json:"fields"`
+	PlayerSymbols []string `json:"playerSymbols"`
+	Started       bool     `json:"started"`
+	//These are not exported to JSON
+	numberOfPlayers int `json:"-"`
+	playersTurn     int `json:"-"`
+	numberOfMoves	int `json:"-"`
 }
 
 type field struct {
@@ -43,16 +44,47 @@ func (gs *gameState) addPlayer() {
 	gs.numberOfPlayers++
 	switch gs.numberOfPlayers {
 	case 1:
-		gs.StatusMessage = "waiting for second player..."
+		gs.StatusMessage = "Waiting to get paired"
 	case 2:
-		gs.StatusMessage = "game begins!"
+		gs.StatusMessage = "Game begins!"
 		gs.Started = true
 	}
 }
 
 func (gs *gameState) makeMove(playerNum int, field int) {
-	gs.Fields[field].Set = true
-	gs.Fields[field].Symbol = gs.PlayerSymbols[playerNum] //X atm
+	if gs.isPlayersTurn(playerNum){
+		if gs.isLegalMove(field){
+			gs.Fields[field].Set = true
+			gs.Fields[field].Symbol = gs.PlayerSymbols[playerNum]
+			gs.switchPlayersTurn(playerNum)
+			gs.numberOfMoves++
+			gs.checkForDraw()
+		}
+	}
+}
+
+func (gs *gameState) checkForDraw(){
+	//Todo: Implement in a gooooood way plz
+	if gs.numberOfMoves == 9{
+		gs.StatusMessage = "Draw!"
+	}
+}
+
+func (gs *gameState) isLegalMove(field int) bool{
+	return !gs.Fields[field].Set
+}
+
+func (gs *gameState) isPlayersTurn(playerNum int) bool{
+	return playerNum == gs.playersTurn
+}
+
+func (gs *gameState) switchPlayersTurn(playerNum int) {
+	switch playerNum {
+	case 0:
+		gs.playersTurn = 1
+	case 1:
+		gs.playersTurn = 0
+	}
 }
 
 func stateToJSON(gs gameState) []byte {
--- webroot/app.js
+++ webroot/app.js
@@ -1,9 +1,23 @@
+Vue.config.debug = true;
+
 var tictactoe = new Vue({
     el: '#tictactoe',
     data: {
         gameState: {
             started: false,
-        }
+            fields: [],
+        },
+    },
+    computed: {
+        row1: function() {
+            return this.gameState.fields.slice(0,3);
+        },
+        row2: function() {
+            return this.gameState.fields.slice(3,6);
+        },
+        row3: function() {
+            return this.gameState.fields.slice(6,9);
+        },
     },
     methods: {
         chooseField: function(fieldNum){
